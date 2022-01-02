fef74660e62736f7b711dacd6842ab7f2267fd1b
JWalker <jeffchongonly@gmail.com>
2022-01-02 15:10:18 +0800

    Init
    Draft version

---
+++ SerilogConsoleAppTemplate/Config.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace SerilogConsoleAppTemplate
+{
+    public static class Config
+    {
+        public static IConfiguration Configuration;
+        public static string ProductionMode { get { return Configuration.GetValue<string>("ProductionMode"); } }
+           
+        public static string SMTP_Server { get { return Configuration.GetValue<string>("Serilog:WriteTo:0:Args:configure:2:Args:mailServer"); } }
+        public static string SMTP_Server_Port { get { return Configuration.GetValue<string>("Serilog:WriteTo:0:Args:configure:2:Args:smtpPort"); } }
+        public static string EmailTo_ForSupport { get { return Configuration.GetValue<string>("EmailTo_ForSupport"); } }
+        public static string EmailCC_ForSupport { get { return Configuration.GetValue<string>("EmailCC_ForSupport"); } }
+        public static string EmailBCC_ForSupport { get { return Configuration.GetValue<string>("EmailBCC_ForSupport"); } }
+        public static string EmailFrom { get { return Configuration.GetValue<string>("EmailFrom"); } }
+        public static string EmailSubject { get { return Configuration.GetValue<string>("EmailSubject"); } }
+        public static string EmailSubject_ForSupport { get { return Configuration.GetValue<string>("EmailSubject_ForSupport"); } }
+        public static string EmailSubject_Alert_ForSupport { get { return Configuration.GetValue<string>("EmailSubject_Alert_ForSupport"); } }
+        public static string EmailDisplayName { get { return Configuration.GetValue<string>("EmailDisplayName"); } }
+        public static string EnableHTML { get { return Configuration.GetValue<string>("EnableHTML"); } }
+        public static string EnableSSL { get { return Configuration.GetValue<string>("EnableSSL"); } }
+        public static string EmailUseNetworkCredential { get { return Configuration.GetValue<string>("EmailUseNetworkCredential"); } }
+        public static string networkCredentialuserName { get { return Configuration.GetValue<string>("Serilog:WriteTo:0:Args:configure:2:Args:networkCredentialuserName"); } }
+        public static string networkCredentialpassword { get { return Configuration.GetValue<string>("Serilog:WriteTo:0:Args:configure:2:Args:networkCredentialpassword"); } }
+
+    }
+}
---
+++ SerilogConsoleAppTemplate/Helper.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace SerilogConsoleAppTemplate
+{
+    public class EmailStyles
+    {
+        public const string STYLE_EMAIL_COMMON = "<style>table {border-spacing: 0;border-collapse: collapse;font-family: 'Segoe UI',Helvetica,Arial,sans-serif; font-size:1em;}td, th {border: 1px solid green;text-align: center;padding: 3px;}td {text-align: left;padding: 3px;}</style>";
+        public const string STYLE_EMAIL_BODY_COMMON = "<body style=\"font-family: 'Segoe UI',Helvetica,Arial,sans-serif; font-size:1em;\">";
+    }
+
+    public class Helper
+    {
+        private readonly ILogger<Helper> _log;
+
+        public Helper(ILogger<Helper> log, IConfiguration config)
+        {
+            this._log = log;
+            Config.Configuration = config;
+        }
+
+
+
+        public bool NotifyEmail_ToSupport(string sEmailSubject, string sEmailBodyContent, Dictionary<string, string> sFile)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient(Config.SMTP_Server);
+                mail.From = new MailAddress("\"" + Config.EmailDisplayName + "\"" + Config.EmailFrom);
+
+                //Email to person
+                foreach (var address in Config.EmailTo_ForSupport.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                    mail.To.Add(address);
+
+                if (Config.EmailCC_ForSupport != "")
+                {
+                    foreach (var address in Config.EmailCC_ForSupport.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                        mail.CC.Add(address);
+                }
+
+                if (Config.EmailBCC_ForSupport != "")
+                {
+                    foreach (var address in Config.EmailBCC_ForSupport.Split(new[] { "," }, StringSplitOptions.RemoveEmptyEntries))
+                        mail.Bcc.Add(address);
+                }
+
+                mail.Subject = sEmailSubject;
+                //mail.Body = sEmailBody;
+                if (Config.EnableHTML == "1")
+                    mail.IsBodyHtml = true;
+                else
+                    mail.IsBodyHtml = false;
+
+                foreach (var sFileAttach in sFile)
+                {
+                    Attachment attachment;
+                    attachment = new System.Net.Mail.Attachment(sFileAttach.Key);
+                    attachment.Name = sFileAttach.Value;
+                    mail.Attachments.Add(attachment);
+                }
+
+                var view = AlternateView.CreateAlternateViewFromString(sEmailBodyContent, null, "text/html");
+                mail.AlternateViews.Add(view);
+
+                SmtpServer.Port = Config.SMTP_Server_Port == "" ? 25 : Config.SMTP_Server_Port.ToInt32();
+                if (Config.EmailUseNetworkCredential == "1")
+                    SmtpServer.Credentials = new System.Net.NetworkCredential(Config.networkCredentialuserName, Config.networkCredentialpassword);
+
+                if (Config.EnableSSL == "1")
+                    SmtpServer.EnableSsl = true;
+                else
+                    SmtpServer.EnableSsl = false;
+
+                SmtpServer.Send(mail);
+
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _log.LogError(ex.Message);
+                return false;
+            }
+        }
+    }
+}
